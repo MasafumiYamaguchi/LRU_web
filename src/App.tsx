8c7f0f58371ad2a750ae7e7148dba0d56ab90e59
Masafumi Yamaguchi <al24031@shibaura-it.ac.jp>
2025-06-20 16:32:14 +0900

    ページフォルトの状態管理を追加し、表示を更新

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,30 @@ function App() {
   const [clock, setClock] = useState(0);
   const [inputId, setInputId] = useState('');
   const [list, setList] = useState<DataItem[]>([]);
+  const [isPagefault, setIsPagefault] = useState(false); // 追加
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
     setList(prev => {
-      // 既存のデータがある場合は、clockを更新
       const existIndex = prev.findIndex(item => item.id === inputId);
       if (existIndex !== -1) {
         const updated = [...prev];
         updated[existIndex] = { id: inputId, clock: clock + 1 };
+        setIsPagefault(false); // 既存ページはフォルトしない
         return updated;
       }
-
       // 空きがあれば追加
       if (prev.length < MAX_LENGTH) {
+        setIsPagefault(true); // ページフォルト
         return [...prev, { id: inputId, clock: clock + 1 }];
       }
-
       // 一番古いデータを削除して新しいデータを追加
       const minClock = Math.min(...prev.map(item => item.clock));
       const minIndex = prev.findIndex(item => item.clock === minClock);
       const replaced = [...prev];
       replaced[minIndex] = { id: inputId, clock: clock + 1 };
+      setIsPagefault(true); // ページフォルト
       return replaced;
     });
     setClock(prev => prev + 1);
@@ -60,6 +62,7 @@ function App() {
         </form>
       </div>
       <Data list={list} />
+      {isPagefault ? (<h2 className='pagefault'>Page Fault</h2>) : null}
     </>
   )
 }
